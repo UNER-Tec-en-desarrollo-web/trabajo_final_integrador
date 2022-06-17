2c016c27d32684c65a2f7038af5fa744052eadc7
MartinJauregui19 <guido_martin_jauregui19@gmail.com>
2022-06-17 20:59:13 -0300

    Funcionalidades de escalon 8

--- juego.py
+++ juego.py
@@ -4,28 +4,24 @@ from administrador_datos import cargar_datos
 from  jugador import *
 
 def jugar():
-    print()
-    print("Escalon N°", jugador["escalon"], "!!!!!")
-    print(jugador["nombre"], "Puntaje:", jugador["puntaje"])
     pregunta = nueva_pregunta()
-    print(pregunta["pregunta"])
 
-    mostrar_opciones(pregunta)
-
-    respuesta = input()
-
-    if respuesta.lower() == pregunta["correcta"].lower():
-        print("Es correcto")
-        sumar_puntaje()
-        subir_escalon()
-        jugar()
-    else:
-        print("Es Incorrecto")
-        if jugador["escalon"] != 8:
+    if jugador["escalon"] != 8:
+        print()
+        print("Escalon N°", jugador["escalon"], "!!!!!")
+        print(jugador["nombre"], "Puntaje:", jugador["puntaje"])
+    
+        if es_correcta(pregunta): # hace la pregunta y verifica y se contesta correctamente
+            print("Es correcto")
+            sumar_puntaje()
+            subir_escalon()
             jugar()
         else:
-            fin_juego()
-            cargar_datos()
+            print("Es Incorrecto")
+            subir_escalon()           
+            jugar()
+    else:
+        escalon_8(pregunta)
             
 
 def nueva_pregunta():
@@ -34,11 +30,47 @@ def nueva_pregunta():
     return lista_preguntas[randint(0,len(lista_preguntas) - 1)]
 
 def mostrar_opciones(pregunta):
+    num_opcion = 1
     for i in pregunta["opciones"]:
-        print(i)
+        print(num_opcion, i)
+        num_opcion += 1
 
 def fin_juego():
-    print("Fin del juego")
-    jugador["nombre"]
-    jugador["puntaje"]
-    jugador["puntaje"]
+    print("Fin del juego\n")
+    
+    print("FELICIDADES!!!\nOBTUVISTE PUNTAJE PERFECTO:", jugador["puntaje"]) if jugador["puntaje"] == 110 else print("Puntaje final:", jugador["puntaje"])
+    
+
+def es_correcta(pregunta):
+    print(pregunta["pregunta"])
+    mostrar_opciones(pregunta)
+    respuesta = int(input())
+    while respuesta < 1 or respuesta > 4:
+        print("Opción incorrecta!!!")
+        print()
+        print(pregunta["pregunta"])
+        mostrar_opciones(pregunta)
+        respuesta = int(input())
+
+    return pregunta["correcta"] == pregunta["opciones"][respuesta - 1]
+
+def escalon_8(pregunta):
+    contador = 0
+
+    while contador < 4:
+        print()
+        print("Escalon N°", jugador["escalon"], " Final Boss!!!!!")
+        print(jugador["nombre"], "Puntaje:", jugador["puntaje"])
+        print("Contesta 4 preguntas para ganarle!!!")
+        print(f"{contador}/4")
+        pregunta = nueva_pregunta()
+        if es_correcta(pregunta): # hace la pregunta y verifica y se contesta correctamente
+            contador += 1   
+            print("Es correcto")
+            sumar_puntaje()
+        else:
+            print("Es Incorrecto")      
+            contador = 5
+
+    fin_juego()
+    cargar_datos()
